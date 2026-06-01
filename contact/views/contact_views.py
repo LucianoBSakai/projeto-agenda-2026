7479e07c3ccbb449fb519b215b3cccea988459e7
Luciano Barbosa Sakai <lb.sakai@gmail.com>
2026-05-31 21:17:22 -0300

    iniciando a create contact, tambem foi finalizado a paginaçao com o uso do paginator do Django

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.core.paginator import Paginator
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
@@ -7,10 +8,14 @@ from contact.models import Contact
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[10:20]
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -20,25 +25,31 @@ def index(request):
         context
     )
 
+
 def search(request):
-    search_value = request.GET.get('q', '').strip() 
+    search_value = request.GET.get('q', '').strip()
 
     if search_value == '':
         return redirect('contact:index')
-    
+
     contacts = Contact.objects \
         .filter(show=True)\
         .filter(
             Q(first_name__icontains=search_value) |
             Q(last_name__icontains=search_value) |
             Q(phone__icontains=search_value) |
-            Q(email__icontains=search_value) 
-            )\
+            Q(email__icontains=search_value)
+        )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
-        'site_title': 'Contatos - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
@@ -57,7 +68,7 @@ def contact(request, contact_id):
 
     context = {
         'contact': single_contact,
-        'site_title': site_title,
+        'site_title': site_title
     }
 
     return render(
